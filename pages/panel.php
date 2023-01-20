bc9eabdf725796ba2e5f2748ab00a5edcba79feb
su apt install kuma <maxxozou@gmail.com>
2023-01-20 11:50:10 +0100

    Version 1.1

---
+++ pages/panel.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../style/panel.css">
+
+    <title>Bonjour, utilisateur</title>
+</head>
+<body>
+    <nav>
+        <h1>Tuto PHP</h1>
+
+        <ul class="list">
+            <a href="#">Accueil</a>
+            <a href="#">Voir les utilisateurs</a>
+            <a href="#">Se déconnecter</a>
+        </ul>
+    </nav>
+</body>
+</html>
